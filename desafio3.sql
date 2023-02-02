918e11e799792029b89b374e62e65055271845f6
Marinna Vitoriano <marinna@outlook.com>
2023-02-02 14:52:35 -0300

    Requisito 03 - Exibe o histórico de reprodução

---
+++ desafio3.sql
@@ -0,0 +1,19 @@
+SELECT
+	u.user_name 'usuario',
+    COUNT(r.user_id) 'qt_de_musicas_ouvidas',
+    ROUND(SUM(s.song_duration) / 60, 2) 'total_minutos'
+FROM
+	SpotifyClone.users u
+INNER JOIN
+	SpotifyClone.reprodution_history r
+ON
+	r.user_id = u.user_id
+INNER JOIN
+	SpotifyClone.songs s
+ON
+	s.song_id = r.song_id
+GROUP BY
+	u.user_name
+ORDER BY
+	u.user_name;
+  
